fb4bd1f34e608e0c33e92f378f5963591b8689bb
MCTF-Alpha-27 <j13816180940@139.com>
2022-08-02 12:30:34 +0800

    添加项目文件。

---
+++ IPlugin.cs
@@ -0,0 +1,25 @@
+﻿namespace PluginFrame
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// 插件名称
+        /// </summary>
+        string PluginName { get; }
+
+        /// <summary>
+        /// 插件版本
+        /// </summary>
+        string PluginVersion { get; }
+
+        /// <summary>
+        /// 插件介绍
+        /// </summary>
+        string PluginDescription { get; }
+
+        /// <summary>
+        /// 插件入口方法
+        /// </summary> 
+        void Main();
+    }
+}
