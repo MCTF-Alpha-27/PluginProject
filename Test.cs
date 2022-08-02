cb6111ff7b4f2ac9aeb588d3df82c0ba70115bd7
MCTF-Alpha-27 <j13816180940@139.com>
2022-08-02 13:29:37 +0800

    添加项目文件。

---
+++ Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PluginFrame;
+
+namespace TestPlugin
+{
+    public class Test : IPlugin
+    {
+        public string PluginName => "TestPlugin";
+
+        public string PluginVersion => "1.0";
+
+        public string PluginDescription => "A test plugin";
+
+        public void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
