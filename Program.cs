70cd889757664f0d64331eb4340abae71fed4d21
MCTF-Alpha-27 <j13816180940@139.com>
2022-08-02 12:27:01 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using PluginsService;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Plugins plugins = new("plugins");
+        Console.WriteLine("插件名称：" + plugins.GetPluginName("TestPlugin"));
+        Console.WriteLine("插件版本：" + plugins.GetPluginVersion("TestPlugin"));
+        Console.WriteLine("插件介绍：" + plugins.GetPluginDescription("TestPlugin"));
+        plugins.RunPlugin("TestPlugin");
+    }
+}
